d18406bc1781edb174000c8ed73b547846c518f3
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-22 00:23:50 +0100

    Setup gameconfig to store colours used in game - Avoids reusing Rainbow requirments.

--- lib/gameconfig.rb
+++ lib/gameconfig.rb
@@ -1,2 +1,10 @@
 # gameconfig.rb
-# string_frozen_literal: true
+# frozen_string_literal: true
+
+require 'rainbow/refinement'
+using Rainbow
+
+module GameConfig
+  COLOURS = %i[red blue green orange purple yellow].freeze
+  SOLUTION_COLOURS = %i[white black].freeze
+end
