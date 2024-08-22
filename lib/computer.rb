262e529be97c3add0d689688b8552bce301caa19
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-22 21:31:47 +0100

    Set output of computer guess to nil in order to troubleshoot guess.rb.

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,2 +1,14 @@
 # computer.rb
 # frozen_string_literal: true
+
+class Computer
+  attr_writer :computer_guess
+
+  def initialize(computer_args = {})
+    @computer_guess = computer_args[:computer_guess]
+  end
+
+  def log_computer_guess
+    nil
+  end
+end
