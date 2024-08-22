b2d26be39a2ea785c12d5c314d2adf3a2fed7177
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-22 21:46:45 +0100

    Setup class to output 4 random colour variation with repeated colours allowed.

--- lib/computer_solution.rb
+++ lib/computer_solution.rb
@@ -1,2 +1,19 @@
 # computer_solution.rb
 # frozen_string_literal: true
+
+require_relative 'gameconfig'
+
+# Setup ComputerSolution class to output random variation of 4 colours, allowing repeated colours.
+class ComputerSolution
+  include GameConfig
+
+  attr_writer :computer_solution
+
+  def initialize
+    @computer_solution = computer_solution
+  end
+
+  def computer_solution
+    4.times.map { COLOURS.sample }
+  end
+end
