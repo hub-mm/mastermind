e9f5afe3ef00ec08c0759b216d70eafe58907432
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-22 18:41:59 +0100

    Add ability to name human.

--- lib/human.rb
+++ lib/human.rb
@@ -1,2 +1,20 @@
 # human.rb
 # frozen_string_literal: true
+
+class Human
+  attr_accessor :name, :guess_human
+
+  def initialize(args_human = nil)
+    @name = args_human[:name]
+    @human_guess = args_human[:human_guess]
+  end
+
+  def human_name
+    print 'Enter your name: '
+    @name = gets.chomp
+  end
+end
+
+# Test and troubleshoot
+name = Human.new(name: 'Human')
+name.human_name
