989ce700867347d751f5a3ff83ad72f79204842b
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-22 22:39:44 +0100

    Setup solution class to determine who the solution is coming from.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,29 @@
 # solution.rb
 # frozen_string_literal: true
 
+require_relative 'computer_solution'
+require_relative 'human_solution'
+
+# Setup Solution class to check which if it is receiving solution from computer or human.
+class Solution
+  attr_reader :computer_solution, :human_solution
+  attr_writer :solution
+
+  def initialize
+    @computer = ComputerSolution.new
+    @human = HumanSolution.new(human_solution)
+    @computer_solution = @computer.log_computer_solution
+    @human_solution = @human.log_human_solution
+    @solution = solution
+  end
+
+  def solution
+    if @human_solution.nil?
+      puts 'Loggin computer solution'
+      @computer_solution
+    elsif @computer_solution.nil?
+      puts 'Loggin computer solution'
+      @human_solution
+    end
+  end
+end
