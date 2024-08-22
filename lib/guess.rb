a655c9909add5408a4b09c6987ab0f37189ba119
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-22 21:32:59 +0100

    Fix bugs in how guesses from computer and human are accessed.

--- lib/guess.rb
+++ lib/guess.rb
@@ -1,2 +1,36 @@
 # guess.rb
 # frozen_string_literal: true
+
+require_relative 'computer'
+require_relative 'human'
+
+class Guess
+  attr_reader :computer_guess, :human_guess
+  attr_writer :guess
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+    @human_guess = @human.log_human_guess
+    @computer_guess = @computer.log_computer_guess
+    @guess = guess
+  end
+
+  def guess
+    if @human_guess.nil?
+      puts 'Logging computer guess'
+      @guess = @computer_guess
+    elsif @computer_guess.nil?
+      puts 'Logging human guess'
+      @guess = @human_guess
+      puts @human_guess
+    end
+  end
+end
+
+# Test and troubleshoot
+# human = Human.new
+# human.log_human_name
+# human.log_human_guess
+who_guessing = Guess.new
+who_guessing.guess
