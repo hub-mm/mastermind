fd9326e50a0031cbf39be3ea7a6fdacde278091c
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-23 22:41:10 +0100

    Random selection of two colours repeated in arr eg: [x,x,y,y].

--- lib/computer.rb
+++ lib/computer.rb
@@ -6,6 +6,12 @@ class Computer
   attr_writer :computer_guess
 
   def log_computer_guess
-    nil
+    colours = %w[red blue green orange purple yellow]
+    match = 0
+    random_colour = colours.sample(2)
+    computer_guess = [random_colour[0], random_colour[0], random_colour[1], random_colour[1]]
   end
 end
+
+arr = Computer.new
+p arr.log_computer_guess
