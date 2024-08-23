92986dd93d9ae2867e34918fe6273d6116769717
hub-mm <165568972+hub-mm@users.noreply.github.com>
2024-08-23 21:33:49 +0100

    Make functionality to add name and exit game.

--- lib/game.rb
+++ lib/game.rb
@@ -16,6 +16,7 @@ class Game
   def initialize
     @solution = nil
     @guess = nil
+    human_name
     who_is_playing
     @board = Board.new(solution: @solution, guess: @guess)
   end
@@ -44,24 +45,38 @@ class Game
 
   private
 
-  # rubocop: disable Metrics/MethodLength
-  # rubocop: disable Layout
+  def human_name
+    print 'Hey, nice to see you\'ve decided to try this game of Mastermind. What should I call you: '
+    name = gets.chomp.downcase
+    case name
+    when nil
+      'Human'
+    when 'exit'
+      puts 'Thank you for playing!'
+      exit
+    end
+  end
+
+  # rubocop: disable Metrics
   def who_is_playing
-    puts 'Who\'s playing? If you want to crack the code enter: \'human\'; if you want to make the code enter: \'computer\'.'
-    current_player = gets.chomp
-    if current_player == 'human'
+    print 'Who\'s playing? If you want to crack the code enter: \'human\', if you want to make the code: \'computer\': '
+    current_player = gets.chomp.downcase
+    case current_player
+    when 'human'
       @solution = ComputerSolution.new.log_computer_solution
       @guess = Human.new.log_human_guess
-    elsif current_player == 'computer'
+    when 'computer'
       @solution = HumanSolution.new.log_human_solution
       @guess = Computer.new.log_computer_guess
+    when 'exit'
+      puts 'Thank you for checking this game out!'
+      exit
     else
       puts 'Invalid input. Enter either \'human\' or \'computer\'.'
       who_is_playing
     end
   end
-  # rubocop: enable Layout
-  # rubocop: enable Metrics/MethodLength
+  # rubocop: enable Metrics
 
   def display_board
     @board.board
@@ -76,6 +91,3 @@ class Game
     end
   end
 end
-
-game = Game.new
-game.play
--- lib/human.rb
+++ lib/human.rb
@@ -5,14 +5,8 @@
 class Human
   attr_accessor :name, :human_guess
 
-  def log_human_name
-    print 'Enter your name: '
-    @name = gets.chomp
-    @name = 'Human' if @name.empty? || @name.nil?
-  end
-
   def log_human_guess
     print 'Enter 4 colours: '
-    gets.chomp.split(' ')
+    @human_guess = gets.chomp.split(' ')
   end
 end
